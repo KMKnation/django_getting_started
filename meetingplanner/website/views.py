68f9d1d14996d2c5e4ea0565a3b2f11298d71292
Mayur Kanojiya <kanojiyamayur@gmail.com>
2020-04-22 17:55:31 +0530

    Added date url

--- meetingplanner/website/views.py
+++ meetingplanner/website/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
 def welcome(request):
     return HttpResponse("Welcome to the meeting planner")
+
+def date(request):
+    return HttpResponse("You have load the page at "+str(datetime.now()))
