1f0b469662833bf2829fa927367646190e1bf3c0
Mayur <mayur.kanojiya@hiddenbrains.in>
2020-04-24 16:17:30 +0530

    Link Room as reference to meeting

--- meetingplanner/meetings/models.py
+++ meetingplanner/meetings/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 from datetime import time
 
+
+class Room(models.Model):
+    name = models.CharField(max_length=30)
+    floor_num = models.IntegerField()
+    room_num = models.IntegerField()
+
+    def __str__(self):
+        return str(self.name) + " in " + str(self.room_num) + " room at " + str(self.floor_num) + " floor"
+
 # Create your models here.
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.title) + " at "+str(self.date)
+        return str(self.title) + " at " + str(self.date)
 
-class Room(models.Model):
-    name = models.CharField(max_length=30)
-    floor_num = models.IntegerField()
-    room_num = models.IntegerField()
