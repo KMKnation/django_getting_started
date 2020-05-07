ff415486fbb4fb7006aa6d8dd0dde4df5778ee0f
Mayur Kanojiya <kanojiyamayur@gmail.com>
2020-05-07 18:35:58 +0530

    feat: Learning to pass the data from python views to templates html files

--- meetingplanner/website/views.py
+++ meetingplanner/website/views.py
@@ -5,7 +5,8 @@ from datetime import datetime
 # Create your views here.
 
 def welcome(request):
-    return render(request, 'website/welcome.html')
+    return render(request, 'website/welcome.html',
+                  {"message": "This line comes from python views by passing context !!"})
 
 def date(request):
     return HttpResponse("You have load the page at "+str(datetime.now()))
