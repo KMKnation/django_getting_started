a83290098ded0cbff0196d7a21c7b013bc05b1e9
Mayur Kanojiya <kanojiyamayur@gmail.com>
2020-05-07 19:55:33 +0530

    feat: Added dynamic details page of meeting

--- meetingplanner/meetings/views.py
+++ meetingplanner/meetings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from meetings.models import Meeting
 
 # Create your views here.
+
+def detail(request, id):
+    return render(request,
+                  'meetings/detail.html',
+                  {'meeting': Meeting.objects.get(pk=id)})
--- meetingplanner/website/views.py
+++ meetingplanner/website/views.py
@@ -2,11 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
 
+from meetings.models import Meeting
+
 # Create your views here.
 
 def welcome(request):
     return render(request, 'website/welcome.html',
-                  {"message": "This line comes from python views by passing context !!"})
+                  {"meetings_count": Meeting.objects.count()})
 
 def date(request):
     return HttpResponse("You have load the page at "+str(datetime.now()))
